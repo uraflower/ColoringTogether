d8b12c4438f2effd58350f54f55f51e0eee7df47
Lee Mirim <uraflower@naver.com>
2023-04-03 01:08:39 +0900

    socket.io를 이용해 서버단에서의 basic chatting system 구축

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 5000;
+const server = require('http').createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, { cors: { origin: "*" } });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/chat.html');
+});
+
+io.on('connection', (socket) => {
+    //connected log
+    console.log('a user connected');
+
+    //disconnected log
+    socket.on('disconnect', () => {
+        console.log('user discnnected');
+    });
+
+    //chatting log
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
+    });
+});
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Listening on port ${port}`);
-})
+});
