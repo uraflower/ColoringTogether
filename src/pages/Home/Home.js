3de2280b9fb7102be1fb762d5aa4bb7942c3872a
Lee Mirim <uraflower@naver.com>
2023-04-05 04:49:23 +0900

    Home page style 수정

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Home.css";
 
 const Home = () => {
     const navigate = useNavigate();
@@ -20,59 +19,68 @@ const Home = () => {
     }
 
     return (
-        <div className="Home">
+        <div className="flex justify-center items-center flex-col">
             <img
                 id="logo"
-                src={require("../../임시로고이미지.png")} />
-            <form method="get" action="form-action.html">
-                <div className="nickname-container">
-                    <span>닉네임</span>
-                    <label>
-                        <input
-                            type="text"
-                            id="nickname"
-                            name="nickname"
-                            required
-                            value={nickname}
-                            onChange={({ target: { value } }) => setNickname(value)}
-                        />
-                    </label>
+                src={require("../../임시로고이미지.png")}
+                className="mb-10"
+            />
+            <form method="get" action="form-action.html" className="grid gap-4">
+                <div className="grid grid-cols-3 items-center justify-items-center">
+                    <label>닉네임</label>
+                    <input
+                        type="text"
+                        id="nickname"
+                        name="nickname"
+                        className="col-span-2 border border-gray-200 rounded-xl py-2 px-4 text-center"
+                        placeholder="여기에 입력하세요"
+                        required
+                        value={nickname}
+                        onChange={({ target: { value } }) => setNickname(value)}
+                    />
                 </div>
-                <div className="roomtype-container">
-                    <span>방 유형</span>
-                    <input
-                        type="radio"
-                        id="multi"
-                        value="multi"
-                        name="roomtype"
-                        className="peer hidden"
-                        checked={isMulti === true}
-                        onChange={() => setIsMulti(true)}
-                    />
-                    <label
-                        htmlFor="multi"
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        함께하기
-                    </label>
-                    <input
-                        type="radio"
-                        id="solo"
-                        value="solo"
-                        name="roomtype"
-                        className="hidden"
-                        checked={isMulti === false}
-                        onChange={() => setIsMulti(false)}
-                    />
-                    <label
-                        htmlFor="solo"
-                        className="cursor-pointer">
-                        혼자하기
-                    </label>
+                <div className="grid grid-cols-3  items-center justify-items-center">
+                    <span>유형</span>
+                    <div>
+                        <input
+                            type="radio"
+                            id="multi"
+                            value="multi"
+                            name="roomtype"
+                            className="peer hidden"
+                            checked={isMulti === true}
+                            onChange={() => setIsMulti(true)}
+                        />
+                        <label
+                            htmlFor="multi"
+                            className="inline-block cursor-pointer rounded-xl py-2 px-4 border border-gray-200
+                            peer-checked:bg-blue-500 peer-checked:border-blue-500 peer-checked:text-white peer-checked:font-bold">
+                            함께하기
+                        </label>
+                    </div>
+                    <div>
+                        <input
+                            type="radio"
+                            id="solo"
+                            value="solo"
+                            name="roomtype"
+                            className="peer hidden"
+                            checked={isMulti === false}
+                            onChange={() => setIsMulti(false)}
+                        />
+                        <label
+                            htmlFor="solo"
+                            className="inline-block cursor-pointer rounded-xl py-2 px-4 border border-gray-200
+                            peer-checked:bg-blue-500 peer-checked:border-blue-500 peer-checked:text-white peer-checked:font-bold">
+                            혼자하기
+                        </label>
+                    </div>
                 </div>
                 <input
                     type="submit"
                     id="create-room"
                     value="방 생성"
+                    className="cursor-pointer rounded-xl py-3 px-5 text-xl bg-blue-500 text-white"
                     onSubmit={createRoom}
                 />
             </form>
