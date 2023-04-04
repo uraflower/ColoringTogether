95757e7127a35f8a0f83da55251323537027cd82
Lee Mirim <uraflower@naver.com>
2023-04-05 05:48:41 +0900

    client, server 간 socket.io chatting system 연결

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const path = require('path');
 const server = require('http').createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/chat.html');
+    res.sendFile(path.join(__dirname + '../../src/components/chat/chat.html'));
 });
+
 
 io.on('connection', (socket) => {
     //connected log
-    console.log('a user connected');
+    console.log('a user connected:', socket.id);
 
     //disconnected log
     socket.on('disconnect', () => {
-        console.log('user discnnected');
+        console.log('user discnnected:', socket.id);
     });
 
     //chatting log
