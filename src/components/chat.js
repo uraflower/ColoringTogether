203f3c880de8d9b9d830c38d46440b9d48a7cfc9
Lee Mirim <uraflower@naver.com>
2023-04-07 20:55:22 +0900

    chat component style 변경

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
+import './chat.css';
 export const socket = io('localhost:5000');
 export const SocketContext = React.createContext();
 
@@ -19,44 +20,65 @@ const Chat = (props) => {
     const sendMessage = (event) => {
         event.preventDefault();
         const { nickname, message } = chat;
-        socket.emit("chat message", ({ nickname, message }));
-        setChat({ nickname, message: '' });
+
+        if (!message) {
+            return;
+        }
+        else {
+            socket.emit("chat message", ({ nickname, message }));
+            setChat({ nickname, message: '' });
+
+        }
     }
 
     /** 메시지를 수신 */
     const receiveMessage = () => {
         socket.on("chat message", ({ nickname, message }) => {
-            renderChat(nickname, message);
+            addChat(nickname, message);
             console.log(`${nickname}: ${message}`);
         })
     }
 
-    /** 채팅 내역(메시지 기록)을 표시 */
-    const renderChat = (nickname, message) => {
+    /** 채팅 내역(메시지 기록)을 표시
+     * "닉네임: 메시지" 꼴로 표시하며, 닉네임은 bold
+     * 채팅이 입력되면 자동으로 스크롤을 내림
+    */
+    const addChat = (nickname, message) => {
         let chatHistory = document.getElementById('chat-history');
         let item = document.createElement('li');
-
-        item.textContent = `${nickname} : ${message}`;
+        item.innerHTML = `<span id="nickname" style="font-weight:bold;">${nickname}</span>`;
+        item.innerHTML += `: ${message}`;
+        item.className = "py-1 text-sm";
         chatHistory.appendChild(item);
-        window.scrollTo(0, document.body.scrollHeight);
+        chatHistory.scrollTo(0, chatHistory.scrollHeight);
     }
 
     return (
-        <>
-            <ul id="chat-history"></ul>
-            <form onSubmit={sendMessage}>
-                <input
-                    type="text"
+        <div
+            className="flex flex-col fixed top-0 right-0 w-64 h-screen">
+            <ul
+                id="chat-history"
+                className="h-full list-none px-3 py-2 overflow-auto break-words bg-gray-200">
+            </ul>
+            <form
+                id="form"
+                onSubmit={sendMessage}
+                className="flex h-24 box-border p-2 bg-blue-500">
+                <textarea
+                    form="form"
+                    id="message"
                     name="message"
                     value={chat.message}
                     onChange={onTypeMessage}
-                />
+                    maxLength="150"
+                    className="focus:outline-none flex-grow break-words resize-none px-1 text-sm" />
                 <input
                     type="submit"
                     value="Send"
+                    className="focus:outline-none ml-1 p-1 bg-red-400 cursor-pointer"
                 />
             </form>
-        </>
+        </div>
     )
 }
 export default Chat;
