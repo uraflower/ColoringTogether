7d9b24567d0d61cdb0cd0329cd4a841d4f3047b7
uraflower <82873315+uraflower@users.noreply.github.com>
2023-05-17 08:39:11 +0900

    Add _ Increase or decrease brush size

--- src/pages/Coloring/Coloring.js
+++ src/pages/Coloring/Coloring.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import Chat from "../../components/chat";
+import { TbArrowsMove, TbZoomIn, TbZoomOut, TbMinus, TbPlus } from "react-icons/tb";
 
 const Coloring = () => {
     const canvasRef = useRef(null); // canvas는 자체적으로 상태 관리를 함 따로 관리 필요 X
@@ -12,21 +13,37 @@ const Coloring = () => {
         // canvas.height = window.innerHeight;
         const context = canvas?.getContext('2d');
         setContext(context);
+        context.lineWidth = 100;
 
         const image = imageRef.current;
-        image.onload = () => {
-            context.drawImage(image, 0, 0);
-        };
         image.src = '/assets/coloring01.jpg';
-        canvas.width = image.width;
-        canvas.height = image.height;
+        // 여기는 그냥 캔버스 크기!
+        canvas.height = window.innerHeight * 0.8;
+        canvas.width = image.width / image.height * canvas.height;
 
+        image.onload = () => {
+            // 여기서 이미지 크기를 조정
+            context.drawImage(image, 0, 0, canvas.width, canvas.height);
+        };
     }, []);
 
+    const decreaseBrushSize = () => {
+        context.lineWidth -= 1;
+    };
+
+    const increaseBrushSize = () => {
+        context.lineWidth += 1;
+    };
 
     return (
         <>
             <header className="bg-amber-400 p-2 space-x-1">
+                <button onClick={decreaseBrushSize}>
+                    <TbMinus />
+                </button>
+                <button onClick={increaseBrushSize}>
+                    <TbPlus />
+                </button>
             </header>
             <div className="bg-gray-200 w-full flex">
                 <main className="bg-red-200"
